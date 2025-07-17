b98d8a273a6a264bc69af110dd1fed9e5bc1ba3b
lottie-icm <landes@theicm.org>
2025-07-17 16:54:55 -0500

    Small color changes

--- global/global.gd
+++ global/global.gd
@@ -22,7 +22,7 @@ var selected_tool : ToolType = ToolType.CRAYON
 var active_color : Color
 var tool_size : int
 var background_color : Color = "2f589e" #set via palette and load palette in editor not drawing tools
-
+#1f2757
 func get_drawing_color() -> Color:
 	if selected_tool == ToolType.ERASER:
 		return background_color
