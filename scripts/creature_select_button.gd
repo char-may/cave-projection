52b8ab7105fa92044a9aa136b7e4b37bd9dba55d
lottie-icm <landes@theicm.org>
2025-08-08 16:21:06 -0500

    Added to creature select functionality

--- scripts/creature_select_button.gd
+++ scripts/creature_select_button.gd
@@ -3,12 +3,21 @@ class_name CreatureSelectButton extends TextureButton
 @export var creature_type : Global.CreatureType
 @export var frame_a : Texture2D
 @export var frame_b : Texture2D
+@export var bat_button : CreatureSelectButton
+@export var tardigrade_button : CreatureSelectButton
+@export var salamander_button : CreatureSelectButton
+@export var monster_button : CreatureSelectButton
+
+@onready var grid = $".."
+@onready var confirm_label = $"../../ConfirmLabel"
 
 var current_frame : Texture2D
+
 
 func _ready() -> void:	
 	current_frame = frame_a
 	texture_normal = current_frame
+	confirm_label.visible = false
 
 func _process(_delta: float) -> void:
 	pass
@@ -22,4 +31,47 @@ func _on_timer_timeout() -> void:
 	texture_normal = current_frame
 
 func _on_pressed() -> void:
-	pass # Replace with function body.
+	match creature_type:
+		Global.CreatureType.BAT:
+			bat_selected()
+		Global.CreatureType.TARDIGRADE:
+			tardigrade_selected()
+		Global.CreatureType.SALAMANDER:
+			salamander_selected()
+		Global.CreatureType.MONSTER:
+			monster_selected()
+
+func bat_selected():
+	tardigrade_button.visible = false
+	salamander_button.visible = false
+	monster_button.visible = false
+	grid_tween()
+	grid.columns = 1
+	grid_tween()
+	
+func tardigrade_selected():
+	bat_button.visible = false
+	salamander_button.visible = false
+	monster_button.visible = false
+	grid.columns = 1
+	grid_tween()
+
+func salamander_selected():
+	bat_button.visible = false
+	tardigrade_button.visible = false
+	monster_button.visible = false
+	grid.columns = 1
+	grid_tween()
+
+func monster_selected():
+	bat_button.visible = false
+	tardigrade_button.visible = false
+	salamander_button.visible = false
+	grid.columns = 1
+	grid_tween()
+
+func grid_tween():
+	var tween = grid.create_tween()
+	tween.tween_property(grid, "rotation_degrees", 360.0, .7).set_trans(Tween.TRANS_EXPO)
+	await get_tree().create_timer(.5).timeout
+	confirm_label.visible = true
