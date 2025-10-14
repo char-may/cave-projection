862ec7c19374b8fd837382a6fdb48f980d99abbd
lottie-icm <landes@theicm.org>
2025-10-14 11:15:13 -0500

    Remove creature resource type

--- Resources/Creatures/Creature.gd
+++
@@ -1,7 +0,0 @@
-class_name Creature
-
-@export var type : Global.CreatureType
-@export var palette : Palette
-@export var drawing_mask : CompressedTexture2D
-@export var texture : CompressedTexture2D
-@export var secret : String
