174950af90397db4c427212af8feca78fec26767
lottie-icm <landes@theicm.org>
2025-10-14 16:17:41 -0500

    Edited face art and random face working

--- scenes/face_generator.gd
+++ scenes/face_generator.gd
@@ -9,7 +9,16 @@ extends Node2D
 @export var nose : Sprite2D
 @export var mouth : Sprite2D
 
-
-#Set random texture
-#var random_index = randi_range(0, textures.size() - 1)
-#$Sprite2D.texture = textures[random_index]
+func _process(_delta):
+	if Input.is_action_just_pressed("generate_face"):
+		print("Make a silly face!")
+		#Set random eyes
+		var random_eye_index = randi_range(0, eye_textures.size() - 1)
+		left_eye.texture = eye_textures[random_eye_index]
+		right_eye.texture = eye_textures[random_eye_index]
+		#Set random nose
+		var random_nose_index = randi_range(0, nose_textures.size() - 1)
+		nose.texture = nose_textures[random_nose_index]
+		#Set random mouth
+		var random_mouth_index = randi_range(0, mouth_textures.size() - 1)
+		mouth.texture = mouth_textures[random_mouth_index]
