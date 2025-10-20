5c0e6b30668b7269cc218b32bf15a046b5ff1716
lottie-icm <landes@theicm.org>
2025-10-20 16:52:51 -0500

    Started some scene asset reorganizing
    Try grouping everything into scene folders containing all assets and scripts for that scene

--- scenes/splash_screen/splash_screen_manager.gd
+++ scenes/splash_screen/splash_screen_manager.gd
@@ -24,7 +24,7 @@ func fade() -> void:
 	tween.tween_property(splash_screen, "modulate:a", 0.0, fade_out_time)
 	tween.tween_interval(out_time)
 	await tween.finished
-	Global.game_controller.change_gui_scene("res://scenes/creature_selection.tscn", true, false)
+	Global.game_controller.change_gui_scene("res://scenes/creature_select/creature_select.tscn", true, false)
 	
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_pressed():
