22c1119a6507b8427990a23c690a2ec6d3de6752
lottie-icm <landes@theicm.org>
2026-02-25 12:49:32 -0600

    Started nebula

---
+++ screens/drawing_screen/4_transition_screen/nebula.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@export var rotation_speed = 1.0 # Radians per second
+@onready var nebula_sprite = $Sprite2D
+
+
+# Nebula will play scale in animation, and after a delay play scale out and destroy itself
+# It will always rotate at the set speed
+
+func _process(delta):
+	# Rotate sprite around its own center
+	nebula_sprite += rotation_speed * delta
