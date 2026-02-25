cc5d19f496b1f7f05dc790534ae58e954b4d6a14
lottie-icm <landes@theicm.org>
2026-02-25 14:28:32 -0600

    Added creature destination movement

--- creatures/creature.gd
+++ creatures/creature.gd
@@ -1,6 +1,11 @@
 class_name Creature extends Node2D
 
 @onready var polygons = $Container/Polygons
+@onready var destination_position = $DestinationPosition.global_position
+
+@export var creature_type : Global.CreatureType = Global.CreatureType.BAT
+@export var move_speed : float = 200.0
+@export var moving : bool = false
 
 # constructor
 func _init():
@@ -9,9 +14,19 @@ func _init():
 func _ready():
 	set_texture(Global.finished_atlus)
 
+func _process(delta: float) -> void:
+	# handle movement
+	go_there(delta)
+	
 # set texture of all polygons
 func set_texture(texture: ImageTexture) -> void:
 	if polygons && texture:
 		for p in polygons.get_children():
 			if p is Polygon2D:
 				p.texture = texture
+
+func go_there(delta) -> void:
+	if moving:
+		global_position = global_position.move_toward(destination_position, delta * move_speed)
+	if global_position == destination_position:
+		moving = false
