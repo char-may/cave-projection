988477479fbf15b0468736c7ee793af06119077b
lottie-icm <landes@theicm.org>
2026-03-12 11:54:50 -0500

    Modified some bat dialog

--- creatures/bat/bat.gd
+++ creatures/bat/bat.gd
@@ -1,9 +1,9 @@
 class_name Bat extends Creature
 @onready var label = $Container/Label
 
-var label_text: Array = ["!", "?", "hi!", "oh hi!", "I'm a bat!", "Pardon me", "Sup",
-"Where are we?", "Nice drawing!", "Have you seen Pete?"]
-
+var label_text: Array = ["!", "?", "hi!", "oh hi!", "pardon me", "sup...", "nice drawing!", "have you seen pete?", "walking feet!",
+"hey buddy!", "hiya!", "where are we?", "nice cave isn't it?" ]
+var talking : bool = false
 var rng #random number generator
 
 func _ready():
@@ -13,11 +13,16 @@ func _ready():
 	label.visible = false
 	
 func _on_area_2d_area_entered(_area: Area2D) -> void:
-	var random_time = rng.randf_range(0.1, .5)
-	var random_text: String = label_text.pick_random()
-	await get_tree().create_timer(random_time).timeout
-	label.text = random_text
-	label.visible = true
-	random_time = rng.randf_range(1.0, 3.0)
-	await get_tree().create_timer(random_time).timeout
-	label.visible = false
+	if !talking:
+		talking = true
+		var random_time = rng.randf_range(0.1, .5)
+		var random_text: String = label_text.pick_random()
+		await get_tree().create_timer(random_time).timeout
+		label.text = random_text
+		label.visible = true
+		random_time = rng.randf_range(1.0, 3.0)
+		await get_tree().create_timer(random_time).timeout
+		label.visible = false
+		talking = false
+	else:
+		pass
