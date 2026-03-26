3459981f23ec28f9875fc7385a4555ad68ca5823
lottie-icm <landes@theicm.org>
2026-03-26 18:45:51 -0500

    Moving random texture stuff to global

--- global_scripts/global.gd
+++ global_scripts/global.gd
@@ -29,3 +29,58 @@ func get_drawing_color() -> Color:
 		return background_color
 	else:
 		return active_color
+		
+func select_random_texture(type: String):
+	# sets Global.finished_atlas to a random file from creature directory
+	# currently using input but may need to be a signal for auto-populate
+	
+	var creature_images: PackedStringArray = []
+	var path = "user://"
+	var dir = DirAccess.open(path)
+	
+	# get directory
+	match type:
+		"bat":
+			dir.make_dir("exports/bats")
+			dir = DirAccess.open("user://exports/bats/")
+		"tardigrade":
+			dir.make_dir("exports/tardigrades")
+			dir = DirAccess.open("user://exports/tardigrades/")
+		"salamander":
+			dir.make_dir("exports/salamanders")
+			dir = DirAccess.open("user://exports/salamanders/")
+		"bigguy":
+			dir.make_dir("exports/bigguys")
+			dir = DirAccess.open("user://exports/bigguys/")
+		_: #wildcard default bat
+			dir.make_dir("exports/bats")
+			dir = DirAccess.open("user://exports/bats/")
+			
+	dir.list_dir_begin()
+	while true:
+		var file_name = dir.get_next()
+		if file_name == "":
+		# break the while loop when get_next() returns ""
+			break
+		elif !file_name.begins_with("."):
+		# get_next() returns a string so this can be used to load the images into an array.
+			creature_images.append(file_name)
+	dir.list_dir_end()
+	
+	# Get random bat image
+	var random_image_path: String = Array(creature_images).pick_random()
+	
+	# Save image texture to global
+	var image = Image.new()
+	match type:
+		"bat":
+			var load_err := image.load("user://exports/bats/" + random_image_path)
+			assert(load_err == OK, "Failed to load image at: ")
+		"tardigrade":
+			var load_err := image.load("user://exports/tardigrades/" + random_image_path)
+			assert(load_err == OK, "Failed to load image at: ")
+		_:
+			var load_err := image.load("user://exports/bats/" + random_image_path)
+			assert(load_err == OK, "Failed to load image at: ")
+			
+	finished_atlus = ImageTexture.create_from_image(image)
--- screens/cave_screen/cave_screen.gd
+++ screens/cave_screen/cave_screen.gd
@@ -10,10 +10,10 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	if (Input.is_action_just_pressed("create_bat")):
-		select_random_texture("bat")
+		Global.select_random_texture("bat")
 		create_bat()
 	if (Input.is_action_just_pressed("create_tardigrade")):
-		select_random_texture("tardigrade")
+		Global.select_random_texture("tardigrade")
 		create_tardigrade()
 
 func create_bat():
@@ -58,52 +58,3 @@ func create_tardigrade():
 	self.add_child(new_tardigrade)
 	new_tardigrade.destination_position = tardigrade_spawn_node.global_position
 	new_tardigrade.moving = true
-
-func select_random_texture(type: String):
-	# sets Global.finished_atlas to a random file from creature directory
-	# currently using input but may need to be a signal for auto-populate
-	
-	var creature_images: PackedStringArray = []
-	var path = "user://"
-	var dir = DirAccess.open(path)
-	
-	# get directory
-	match type:
-		"bat":
-			dir.make_dir("exports/bats")
-			dir = DirAccess.open("user://exports/bats/")
-		"tardigrade":
-			dir.make_dir("exports/tardigrades")
-			dir = DirAccess.open("user://exports/tardigrades/")
-		_: #wildcard default bat
-			dir.make_dir("exports/bats")
-			dir = DirAccess.open("user://exports/bats/")
-			
-	dir.list_dir_begin()
-	while true:
-		var file_name = dir.get_next()
-		if file_name == "":
-		# break the while loop when get_next() returns ""
-			break
-		elif !file_name.begins_with("."):
-		# get_next() returns a string so this can be used to load the images into an array.
-			creature_images.append(file_name)
-	dir.list_dir_end()
-	
-	# Get random bat image
-	var random_image_path: String = Array(creature_images).pick_random()
-	
-	# Save image texture to global
-	var image = Image.new()
-	match type:
-		"bat":
-			var load_err := image.load("user://exports/bats/" + random_image_path)
-			assert(load_err == OK, "Failed to load image at: ")
-		"tardigrade":
-			var load_err := image.load("user://exports/tardigrades/" + random_image_path)
-			assert(load_err == OK, "Failed to load image at: ")
-		_:
-			var load_err := image.load("user://exports/bats/" + random_image_path)
-			assert(load_err == OK, "Failed to load image at: ")
-			
-	Global.finished_atlus = ImageTexture.create_from_image(image)
