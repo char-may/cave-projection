53f0657abb8ceb39c7132354f0f0655693d22619
lottie-icm <landes@theicm.org>
2026-05-02 12:12:16 -0500

    Started multiple salamanders

--- creatures/salamander/path_follow_2d.gd
+++ creatures/salamander/path_follow_2d.gd
@@ -1,6 +1,40 @@
 extends PathFollow2D
 
-var speed = 0.2
+@export var timer : Timer
+@onready var salamander = $Salamander
+var speed = 0.1
+var walking : bool = false
+var anim
 
+func _ready() -> void:
+	walking = true
+	anim = salamander.get_node("AnimationPlayer")
+	start_random_timer()
+
+func start_random_timer():
+	timer.wait_time = randf_range(1, 2.5)
+	timer.start()
+	
 func _process(delta) -> void:
-	progress_ratio += delta * speed
+	if walking:
+		progress_ratio += delta * speed
+		
+func _on_timer_1_timeout() -> void:
+	if walking:
+		walking = false
+		anim.stop()
+		start_random_timer()
+	else:
+		walking = true
+		anim.play()
+		start_random_timer()
+		
+func _on_timer_2_timeout() -> void:
+	if walking:
+		walking = false
+		anim.stop()
+		start_random_timer()
+	else:
+		walking = true
+		anim.play()
+		start_random_timer()
